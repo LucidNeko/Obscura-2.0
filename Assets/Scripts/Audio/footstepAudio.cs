0b90dee8d0dd61e83b4e55233d51f4d748443f4a
Guest User <Guest@d-01-06-lab.mddn.vuw.ac.nz>
2015-06-18 14:52:47 +1200

    New foot sounds randomised

--- Assets/Scripts/Audio/footstepAudio.cs
+++ Assets/Scripts/Audio/footstepAudio.cs
@@ -4,17 +4,24 @@ using System.Collections;
 
 [RequireComponent(typeof(AudioSource))]
 
+
+
 public class footstepAudio : MonoBehaviour {
+
+public AudioClip [] clips;
+
 
 
 	AudioSource source; // The audio source that will play the footsteps.
 
 	void Start(){
+		
 		source = GetComponent<AudioSource> (); // Get the audio source object.
 	}
 
 	void OnTriggerEnter(Collider other){
 
+		source.clip = clips[Random.Range(0, 6)];
 		source.Play(); // Tell the audio source to play the clip.
 	}
 }
