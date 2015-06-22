6b9885d675cd8bb6acc98296301788362771a1d2
Hamish <Guest@l-04-320.mddn.vuw.ac.nz>
2015-06-22 23:39:40 +1200

    Add Alter and Lighting Transition

---
+++ Assets/Models/Level Assets/Alter/AlterScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlterScript : MonoBehaviour {
+
+	private Animator m_Animator;
+
+	// Use this for initialization
+	void Start () {
+		m_Animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			m_Animator.SetTrigger("Activate");
+			Camera.main.GetComponent<CameraControls>().Shake(1f, 4f);
+			Camera.main.GetComponent<CameraControls>().LookAt(transform.position, 3, 4f);
+
+			GameObject.Find("GlobalScripts").GetComponent<LightingTransition>().Activate();
+		}
+	}
+}
---
+++ Assets/Scripts/Global/LightingTransition.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LightingTransition : MonoBehaviour {
+
+	public Material m_Skybox1, m_Skybox2;
+
+	public void Activate() {
+		RenderSettings.skybox = RenderSettings.skybox == m_Skybox1 ? m_Skybox2 : m_Skybox1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.N)) {
+			Activate ();
+		}
+	}
+}
