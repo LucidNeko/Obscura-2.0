c3f10053aee76b4fcbf1384f55c3a9e6b9ff60a1
Griffendoors <rlgriffin25@gmail.com>
2015-06-22 20:08:12 +1200

    water respond trigger

---
+++ Assets/Scripts/Player/TriggerDeath.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDeath : MonoBehaviour {
+
+	PlayerControls pc;
+
+	void Start(){
+		pc = GetComponent<PlayerControls> ();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag.Equals ("Death")) {
+			pc.Die();
+		}
+	}
+}
