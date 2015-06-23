43adc88dfeec8d570d1cf738f406fba34fd12620
Hamish <Guest@e-02-03-lab.mddn.vuw.ac.nz>
2015-06-23 19:17:11 +1200

    Add Transitional Objects

---
+++ Assets/Scripts/Global/TransitionableObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TransitionableObject : MonoBehaviour {
+
+	private static List<GameObject> objects = new List<GameObject>();
+
+	public static GameObject[] GetAll {
+		get { return objects.ToArray(); }
+	}
+
+	// Use this for initialization
+	void Start () {
+		objects.Add (gameObject);
+	}
+
+}
