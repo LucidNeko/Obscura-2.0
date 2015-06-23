bd4dee5b0e0c59d9ff33f1e6c5792a1d3f90aee1
Griffendoors <Guest@f-04-06-lab.mddn.vuw.ac.nz>
2015-06-23 20:09:03 +1200

    Big Crystal

---
+++ Assets/BigCrystal.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigCrystal : MonoBehaviour {
+
+
+
+	public GameObject crysM;
+	private CrystalPickup cp;
+
+
+	// Use this for initialization
+	void Start () {
+		cp = crysM.GetComponent<CrystalPickup> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag.Equals ("Player")) {
+			int quantity = cp.GetCrystalsCollected();
+			switch (quantity)
+			{
+			case 0: 
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				StartCoroutine(EnoughCrystals());
+				break;
+
+			}
+		}
+	}
+
+	public IEnumerator EnoughCrystals(){
+		yield return null;
+	}
+
+
+
+}
--- Assets/CrystalPickup.cs
+++ Assets/CrystalPickup.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class CrystalPickup : MonoBehaviour {
 
 	 static int crystalsCollected;
-	
+
 
 	void Start () {
 		crystalsCollected = 0;
